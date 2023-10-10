1a122d56822b03a02197f75c696bb1275abfc694
Sahil Mody <sahilmody1234@gmail.com>
2023-10-10 13:55:11 -0400

    Minor changes

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,5 +1,5 @@
 export const contactsData = {
-    email: 'smody@stevens.edu',
+    email: 'sahilmody749@gmail.com',
     phone: '+1 (551) 234-7138',
     address: 'Jersey City, New Jersey, United States - 07307',
     sheetAPI: 'https://sheetdb.io/api/v1/92mhfs6pgyr34',
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -2,26 +2,26 @@ export const experienceData = [
     {
         id: 1,
         company: 'VteamLabs',
-        jobtitle: 'Software Developer Intern',
+        jobtitle: 'Data Analyst Intern',
         startYear: 'Sep. 2022',
         endYear: 'Dec. 2022',
         duties: <ul>
-                    <li>Implemented 2 features for the OpenEdX course with over 500 students enrolled, using Python, Django, and JavaScript incrementing 35% online learning engagement.</li>
-                    <li>Optimized the existing codebase by refactoring components to uplift performance, applying Agile methodologies and Git which boosted 20% website performance.</li>
-                    <li>Assisted in remodeling a web-based student assessment system which automated the grading process and reduced manual effort by 40%. </li>
+                    <li>Gained proficiency in SQL to query databases efficiently, extract relevant data, and contribute to data extraction and integration processes</li>
+                    <li>Created 8 Power BI dashboards for corporate clients to project Return on Investment, monitor accounts growth, new customer trend and monthly revenue data resulting in 93% customer retention</li>
+                    <li>Generated project reports that effectively communicated data insights to management and the team members</li>
                     <li> Collaborated with business analysts to understand customer needs and develop innovative solutions to enhance customer engagement and satisfaction.</li>
                 </ul>
     },
     {
         id: 2,
         company: 'Trivia Softwares',
-        jobtitle: 'Software Developer Intern',
+        jobtitle: 'Operations Analyst Intern',
         startYear: 'Dec. 2019',
-        endYear: 'Feb 2020',
+        endYear: 'May 2021',
         duties: <ul>
-                    <li>Led a team of 3 and designed an engaging Student Management System applying Tkinter, and worked on Oracle Database to administer database operations on student details.</li>
-                    <li>Scrapped data from various websites adopting APIs and Python libraries such as socket, requests, and beautifulsoup4.</li>
-                    <li>Improved system performance by 8% by working with the testing team to improve the debugging process.</li>
+                    <li>Analyzed 1000+ students historical data using Excel spreadsheet, identified students’ academic performance trends and created quarterly reports for the 9 instructors getting visibility into cohorts’ performance</li>
+                    <li>Presented analytics and findings of students’ and instructors’ performance using graphs to CEO and charts and identifying additional training & development opportunities for 249 underperforming students and 2 teachers</li>
+                    <li>Analyzed historical student performance data and recommended 118 high-performer students for scholarships and admission to Trivia Software’s elite program resulting in achieving 100% success rate for the group</li>
                     {/* <li>Awarded Certificate of Excellence to appreciate my strong leadership, sound character, and passion for innovation.</li> */}
             </ul>
     },
